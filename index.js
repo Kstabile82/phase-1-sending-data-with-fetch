e18890f56daf178f1a634bd410246d27476b538c
KarinaStabile <karina.stabile@gmail.com>
2021-09-16 21:12:20 -0400

    commit tree c26d4f3cc7c9bdbe34b13846987639ec458ecf91

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', { 
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        }, 
+        body: JSON.stringify({
+            name: userName, 
+            email: userEmail, 
+        })
+     })
+
+        .then(function (response) {
+            return response.json();
+        })
+        .then(data => renderId(data))
+        .catch(function (error) {
+           const elem = document.createElement('div');
+           elem.innerHTML = error.message; 
+           document.body.append(elem); 
+        })
+}
+    const renderId = data => {
+        document.body.innerHTML = data.id;
+    }
